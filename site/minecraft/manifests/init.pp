4ea1ddf8c2a3f7b3972669905f6244a7c1c4e5a2
rqtek <Roberto@rqtek.net>
2018-08-31 18:12:10 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -8,7 +8,7 @@ class minecraft (
   file {"${install_dir}/server.jar":
     ensure => file,
     source => $url,
-    befoire => Service['minecraft'],
+    before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
