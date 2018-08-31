f1ea151f4c1097c4662f3bed76f1777185c4e321
rqtek <Roberto@rqtek.net>
2018-08-31 10:42:37 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file {‘/opt/minecraft/server.jar’:
     ensure => file,
-    source      => 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
+    source => 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
   }
   package {‘java’:
     ensure => present,
