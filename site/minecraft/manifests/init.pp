e57f0e4a3e5902bebd608bf945879bc1c1bfdb92
rqtek <Roberto@rqtek.net>
2018-08-31 10:45:36 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,23 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/server.jar’:
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
   }
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true',
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
