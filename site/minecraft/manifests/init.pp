fc5f212de4099ed429f038553bef6b5dfafc9d41
rqtek <Roberto@rqtek.net>
2018-08-31 18:45:43 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,14 +2,13 @@ class minecraft (
   $url = 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
   $install_dir = '/opt/minecraft'
   ){
-  file {'$install_dir::
+  file {$install_dir::
     ensure => directory,
   }
   file {"${install_dir}/server.jar":
     ensure => file,
     source => $url,
-    before => Service['minecraft'],
-      }
+  }
   package {'java':
     ensure => present,
   }
@@ -19,13 +18,12 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-     content => epp('minecraft/minecraft.service', {
+     content => epp('minecraft/minecraft.service.epp', {
      install_dir => $install_dir,
     }),     
   }
   service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
-  }
+ }
 }
